1f55637fcbaf01b3f5d5644671e8e3dab31a02cd
50ShadesOfCode <kaktymail@gmail.com>
2022-07-05 17:07:56 +0300

    Add Github Actions.

---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+import app as tested_app
+import json
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        tested_app.app.config['TESTING'] = True
+        self.app = tested_app.app.test_client()
+    
+if __name__ == '__main__':
+    unittest.main()
